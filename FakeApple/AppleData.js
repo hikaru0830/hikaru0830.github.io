6d9ac84c43137819a3986933f5ce058a91f55580
Gwen <hikar0830@gmail.com>
2023-01-08 21:12:15 +0800

    2023.01.08 更新

---
+++ FakeApple/AppleData.js
@@ -0,0 +1,73 @@
+let iphone = [
+    {productID:1111, productName: "iphone 14 pro", size:6.1, color:"purple", storage: "128GB", price: 34900},
+    {productID:1112, productName: "iphone 14 pro", size:6.1, color:"purple", storage: "256GB", price: 38400},
+    {productID:1113, productName: "iphone 14 pro", size:6.1, color:"purple", storage: "512GB", price: 45400},
+    {productID:1114, productName: "iphone 14 pro", size:6.1, color:"purple", storage: "1TB", price: 52400},
+    {productID:1121, productName: "iphone 14 pro", size:6.1, color:"gold", storage: "128GB", price: 34900},
+    {productID:1122, productName: "iphone 14 pro", size:6.1, color:"gold", storage: "256GB", price: 38400},
+    {productID:1123, productName: "iphone 14 pro", size:6.1, color:"gold", storage: "512GB", price: 45400},
+    {productID:1124, productName: "iphone 14 pro", size:6.1, color:"gold", storage: "1TB", price: 52400},
+    {productID:1131, productName: "iphone 14 pro", size:6.1, color:"silver", storage: "128GB", price: 34900},
+    {productID:1132, productName: "iphone 14 pro", size:6.1, color:"silver", storage: "256GB", price: 38400},
+    {productID:1133, productName: "iphone 14 pro", size:6.1, color:"silver", storage: "512GB", price: 45400},
+    {productID:1134, productName: "iphone 14 pro", size:6.1, color:"silver", storage: "1TB", price: 52400},
+    {productID:1141, productName: "iphone 14 pro", size:6.1, color:"black", storage: "128GB", price: 34900},
+    {productID:1142, productName: "iphone 14 pro", size:6.1, color:"black", storage: "256GB", price: 38400},
+    {productID:1143, productName: "iphone 14 pro", size:6.1, color:"black", storage: "512GB", price: 45400},
+    {productID:1144, productName: "iphone 14 pro", size:6.1, color:"black", storage: "1TB", price: 52400},
+    {productID:1211, productName: "iphone 14 pro Max", size:6.7, color:"purple", storage: "128GB", price: 38900},
+    {productID:1212, productName: "iphone 14 pro Max", size:6.7, color:"purple", storage: "256GB", price: 42400},
+    {productID:1213, productName: "iphone 14 pro Max", size:6.7, color:"purple", storage: "512GB", price: 49400},
+    {productID:1214, productName: "iphone 14 pro Max", size:6.7, color:"purple", storage: "1TB", price: 56400},
+    {productID:1221, productName: "iphone 14 pro Max", size:6.7, color:"gold", storage: "128GB", price: 38900},
+    {productID:1222, productName: "iphone 14 pro Max", size:6.7, color:"gold", storage: "256GB", price: 42400},
+    {productID:1223, productName: "iphone 14 pro Max", size:6.7, color:"gold", storage: "512GB", price: 49400},
+    {productID:1224, productName: "iphone 14 pro Max", size:6.7, color:"gold", storage: "1TB", price: 56400},
+    {productID:1231, productName: "iphone 14 pro Max", size:6.7, color:"silver", storage: "128GB", price: 38900},
+    {productID:1232, productName: "iphone 14 pro Max", size:6.7, color:"silver", storage: "256GB", price: 42400},
+    {productID:1233, productName: "iphone 14 pro Max", size:6.7, color:"silver", storage: "512GB", price: 49400},
+    {productID:1234, productName: "iphone 14 pro Max", size:6.7, color:"silver", storage: "1TB", price: 56400},
+    {productID:1241, productName: "iphone 14 pro Max", size:6.7, color:"black", storage: "128GB", price: 38900},
+    {productID:1242, productName: "iphone 14 pro Max", size:6.7, color:"black", storage: "256GB", price: 42400},
+    {productID:1243, productName: "iphone 14 pro Max", size:6.7, color:"black", storage: "512GB", price: 49400},
+    {productID:1244, productName: "iphone 14 pro Max", size:6.7, color:"black", storage: "1TB", price: 56400},
+];
+
+let ipad = [
+    {productID:2111, productName:"ipad Air", color:"gray", storage:"64GB", internet:"WiFi", price: 19900},
+    {productID:2112, productName:"ipad Air", color:"gray", storage:"64GB", internet:"WiFi-CellularData", price: 24900},
+    {productID:2121, productName:"ipad Air", color:"gray", storage:"256GB", internet:"WiFi", price: 24900},
+    {productID:2122, productName:"ipad Air", color:"gray", storage:"256GB", internet:"WiFi-CellularData", price: 29900},
+    {productID:2211, productName:"ipad Air", color:"blue", storage:"64GB", internet:"WiFi", price: 19900},
+    {productID:2212, productName:"ipad Air", color:"blue", storage:"64GB", internet:"WiFi-CellularData", price: 24900},
+    {productID:2221, productName:"ipad Air", color:"blue", storage:"256GB", internet:"WiFi", price: 24900},
+    {productID:2222, productName:"ipad Air", color:"blue", storage:"256GB", internet:"WiFi-CellularData", price: 29900},
+    {productID:2311, productName:"ipad Air", color:"pink", storage:"64GB", internet:"WiFi", price: 19900},
+    {productID:2312, productName:"ipad Air", color:"pink", storage:"64GB", internet:"WiFi-CellularData", price: 24900},
+    {productID:2321, productName:"ipad Air", color:"pink", storage:"256GB", internet:"WiFi", price: 24900},
+    {productID:2322, productName:"ipad Air", color:"pink", storage:"256GB", internet:"WiFi-CellularData", price: 29900},
+    {productID:2411, productName:"ipad Air", color:"purple", storage:"64GB", internet:"WiFi", price: 19900},
+    {productID:2412, productName:"ipad Air", color:"purple", storage:"64GB", internet:"WiFi-CellularData", price: 24900},
+    {productID:2421, productName:"ipad Air", color:"purple", storage:"256GB", internet:"WiFi", price: 24900},
+    {productID:2422, productName:"ipad Air", color:"purple", storage:"256GB", internet:"WiFi-CellularData", price: 29900},
+    {productID:2511, productName:"ipad Air", color:"starLight", storage:"64GB", internet:"WiFi", price: 19900},
+    {productID:2512, productName:"ipad Air", color:"starLight", storage:"64GB", internet:"WiFi-CellularData", price: 24900},
+    {productID:2521, productName:"ipad Air", color:"starLight", storage:"256GB", internet:"WiFi", price: 24900},
+    {productID:2522, productName:"ipad Air", color:"starLight", storage:"256GB", internet:"WiFi-CellularData", price: 29900},
+
+];
+
+let macBook = [
+    {productID:311, productName:"MacBook Air", wafer:"M1", color:"gray", price:30900},
+    {productID:312, productName:"MacBook Air", wafer:"M1", color:"gold", price:30900},
+    {productID:313, productName:"MacBook Air", wafer:"M1", color:"silver", price:30900},
+    {productID:321, productName:"MacBook Air", wafer:"M2", color:"gray", price:30900},
+    {productID:322, productName:"MacBook Air", wafer:"M2", color:"gold", price:30900},
+    {productID:323, productName:"MacBook Air", wafer:"M2", color:"silver", price:30900}
+];
+
+let productList = [
+    iphone,
+    ipad,
+    macBook 
+]
---
+++ FakeApple/fakeApple.js
@@ -0,0 +1,62 @@
+let iphoneModelArr = [
+    {name:"iPhone 14 Pro", size:6.1, price:34900, model:"normal"},
+    {name:"iPhone 14 Pro Max", size:6.7, price:38900, model:"max"}
+];
+let iphoneColorArr = [
+    {id:"purple", cn:"深紫色", hex:"#635A6D"},
+    {id:"gold", cn:"金色", hex:"#F4E8CE"},
+    {id:"silver", cn:"銀色", hex:"#F1F3F2"},
+    {id:"black", cn:"太空黑色", hex:"#4D4C4A"}
+];
+let iphoneStorageArr = [
+    {storage:"128GB", price:34900},
+    {storage:"256GB", price:38400},
+    {storage:"512GB", price:45400},
+    {storage:"1TB", price:52400}
+]
+
+//IPad
+let ipadColorArr = [
+    {id:"gray", cn:"太空灰色", hex:"#78767B"},
+    {id:"blue", cn:"藍色", hex:"#8FB4C3"},
+    {id:"pink", cn:"粉紅色", hex:"#E8D2CF"},
+    {id:"purple", cn:"紫色", hex:"#BEBDD4"},
+    {id:"starLight", cn:"星光色", hex:"#F4E8CE"}
+]
+
+let ipadStorageArr = [
+    {storage:"64GB", price:19900},
+    {storage:"256GB", price:24900},
+]
+
+let ipadWifiArr = [
+    {id:"only", way:"WiFi", price: 19900},
+    {id:"twoway", way:"WiFi + 行動網路", price: 24900}
+]
+
+//MacBook
+let macBookChipArr = [
+    {chip:"M1", price:30900},
+    {chip:"M2", price:37900}
+]
+
+let macBookM1Color = [
+    {id:"gray", cn:"太空灰色", hex:"#78767B"},
+    {id:"gold", cn:"金色", hex:"#F4E8CE"},
+    {id:"silver", cn:"銀色", hex:"#F1F3F2"},
+];
+let macBookM2Color = [
+    {id:"midnight", cn:"午夜色", hex:"#2E3641"},
+    {id:"starLight", cn:"星光色", hex:"#F4E8CE"},
+    {id:"gray", cn:"太空灰色", hex:"#78767B"},
+    {id:"silver", cn:"銀色", hex:"#F1F3F2"},
+];
+
+let macBookM1Model = [
+    {cpu:8, gpu:7, memory:7, storage:256, price:30900, desc:["16 核心神經網路引擎", "具備原彩顯示的 13 吋 Retina 顯示器", "兩個 Thunderbolt / USB 4 埠", "含 Touch ID 的巧控鍵盤", "力度觸控板", "30W USB-C 電源轉接器"]}
+];
+
+let macBookM2Model = [
+    {cpu:8, gpu:8, memory:8, storage:256, price:37900, desc:["16 核心神經網路引擎", "具備原彩顯示的 13.6 吋 Liquid Retina 顯示器", "1080p FaceTime HD 相機", "MagSafe 3 充電埠", "兩個 Thunderbolt / USB 4 埠", "含 Touch ID 的巧控鍵盤", "力度觸控板", "30W USB-C 電源轉接器"]},
+    {cpu:8, gpu:10, memory:8, storage:512, price:46900, desc:["16 核心神經網路引擎", "具備原彩顯示的 13.6 吋 Liquid Retina 顯示器", "1080p FaceTime HD 相機", "MagSafe 3 充電埠", "兩個 Thunderbolt / USB 4 埠", "含 Touch ID 的巧控鍵盤", "力度觸控板", "35W 雙 USB-C 埠小型電源轉接器"]}
+];
